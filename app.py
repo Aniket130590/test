15b59af1461081159ce699ab3760f43adaaa56db
Aniket130590 <45100319+Aniket130590@users.noreply.github.com>
2022-01-14 13:16:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,101 +2,62 @@ import flask; import plotly.express as px
 import dash; import dash_core_components as dcc; import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
+from fuzzywuzzy import fuzz
 
 
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server)
 
+df=pd.read_csv('consolidated.csv')
 
-#Load csv data 
-df = pd.read_csv('gimnasio.csv') 
-df = df.drop(columns=['Unnamed: 0', 'mes.1', 'mes.2', 'mes.3', 'mes.4'])
-df['date']=pd.to_datetime(df['date'])
-available_indicators = df.columns.unique()
+df["FirstName"] = df["name"].apply(lambda x: x[0:x.find(",")])
+df["LastName"] = df["name"].apply(lambda x: x[x.find(",")+2:])
 
-#Dash code
-app.layout = html.Div([
-    html.Div([html.H1('GYM VS. ECONOMY' ,
-            style={
-            'textAlign': 'center', 'font-family': 'Open Sans',
-            'padding-top':'10px', 'font-weight': '600', 
-            'word-spacing': '5px'
-        }),
-        html.Div([
-            dcc.Dropdown(
-                id='xaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Intensity_economia'
-        )
-        ],
-        style={'width': '45%', 'display': 'inline-block', 'padding': '15px',
-               'font-family': 'Open Sans'}),
+def generate_table(dataframe, max_rows=100):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.loc[:,['name','title','citizenships','addresses','source_list_url','source_information_url']]])] +
 
-        html.Div([
-            dcc.Dropdown(
-                id='yaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Intensity_gimnasio'
-        )
-        ], 
-        style={'width': '45%', 'float': 'right', 'display': 'inline-block', 
-                  'padding': '15px', 'font-family': 'Open Sans'})
-    ]),
-        html.Div([
-        dcc.Graph(
-            id='indicator-graphic'
-        )
-        ], 
-        style={'width':'96%', 'height': '60%', 'display': 'inline-block', 'padding': '15px'}),
-        
-        html.Div([
-        dcc.Graph(id='x-time-series'
-        )
-        ], 
-        style={'width': '46%', 'display': 'inline-block', 'padding': '15px'}),
-  
-        html.Div([
-        dcc.Graph(id='y-time-series'
-        )
-        ],
-        style={'width': '46%', 'display': 'inline-block', 'padding': '15px'})
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.loc[:,['name','title','citizenships','addresses','source_list_url','source_information_url']]
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
 
+app = dash.Dash()
+app.layout = html.Div(
+    children=[html.H4(children='Denied Party Screening'),
+    dcc.Dropdown(
+        id='dropdown',
+        ## extend the options to consider unique Fund values as well
+        #options=[{'label': i, 'value': i} for i in df['Investor'].unique()] + [{'label': i, 'value': i} for i in df['Fund'].unique()],
+        options=[{'label': i, 'value': i} for i in df['FirstName'].unique()] + [{'label': i, 'value': i} for i in df['LastName'].unique()] + [{'label': i, 'value': i} for i in df['name'].unique()],
+        multi=True, placeholder='Filter by Name(FirstName or LastName)...'),
+    html.Div(id='table-container')
+    
+    
 ])
 
-@app.callback(
-    Output('indicator-graphic', 'figure'),
-    [Input('xaxis-column', 'value'),
-     Input('yaxis-column', 'value')])
 
-def update_graph(xaxis_column, yaxis_column):
+@app.callback(dash.dependencies.Output('table-container', 'children'),
+    [dash.dependencies.Input('dropdown', 'value')])
+
+def display_table(dropdown_value):
+    if dropdown_value is None:
+        return generate_table(df)
+
+    ## add an 'or' condition for the other column you want to use to slice the df 
+    ## and update the columns that are displayeds
+    #dff = df[df.name.str.contains('|'.join(dropdown_value)) ]
+    #dff = df[df['name'].str.find('|'.join(dropdown_value)) != -1] 
+    def get_ratio(row):
+        name = row['name']
+        return fuzz.token_sort_ratio(name, '|'.join(dropdown_value))
     
-    fig = px.density_heatmap(df, x=xaxis_column,
-                            y=yaxis_column)
-    return fig
-
-
-@app.callback(
-    Output('x-time-series', 'figure'),
-    [Input('xaxis-column', 'value')])
-
-def update_x_timeseries(xaxis_column):
+    dff = df[df.apply(get_ratio, axis=1) > 60]
     
-    fig = px.scatter(df, x='date', y=xaxis_column,
-                    trendline='lowess', trendline_color_override='crimson')
-    return fig
-
-
-@app.callback(
-    Output('y-time-series', 'figure'),
-    [Input('yaxis-column', 'value')])
-
-def update_y_timeseries(yaxis_column):
-    
-    fig = px.scatter(df, x='date', y=yaxis_column,
-                    trendline='lowess', trendline_color_override='crimson')
-    return fig
-
-
+    dff = dff[['name','title','citizenships','addresses','source_list_url','source_information_url']]
+    return generate_table(dff)
 # Run the Dash app
 if __name__ == '__main__':
     app.server.run(debug=True, use_reloader=False)
